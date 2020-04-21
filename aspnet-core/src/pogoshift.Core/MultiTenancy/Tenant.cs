5ee633ab8751a69e959824ed64df1ac89a740cd9
Joe Boris <josephdboris@gmail.com>
2020-04-21 15:14:24 -0400

    Added custom properties to tenants

--- aspnet-core/src/pogoshift.Core/MultiTenancy/Tenant.cs
+++ aspnet-core/src/pogoshift.Core/MultiTenancy/Tenant.cs
@@ -5,8 +5,16 @@ namespace pogoshift.MultiTenancy
 {
     public class Tenant : AbpTenant<User>
     {
+        public int StoreNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public int DailyAssociateMinimum { get; set; }
+        public int DailyManagerMinimum { get; set; }
+        public string Description { get; set; }
+
         public Tenant()
-        {            
+        {
         }
 
         public Tenant(string tenancyName, string name)

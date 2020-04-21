c4b0a72139d3b66e35227dc4b2ee6454dc811af0
Joe Boris <josephdboris@gmail.com>
2020-04-21 16:35:19 -0400

    User database migration

---
+++ aspnet-core/src/pogoshift.EntityFrameworkCore/Migrations/20200421203453_Add-Data-To-Users.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pogoshift.Migrations
+{
+    public partial class AddDataToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine1",
+                table: "AbpUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine2",
+                table: "AbpUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "AbpUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeNumber",
+                table: "AbpUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "AbpUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine1",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine2",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeNumber",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "AbpUsers");
+        }
+    }
+}

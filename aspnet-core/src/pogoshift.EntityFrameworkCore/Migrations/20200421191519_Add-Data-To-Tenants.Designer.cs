1ccef5b9ec1430fa0c4340e099b4826285b0f87a
Joe Boris <josephdboris@gmail.com>
2020-04-21 16:32:56 -0400

    Tenant database migration

--- aspnet-core/src/pogoshift.EntityFrameworkCore/Migrations/20200421191519_Add-Data-To-Tenants.Designer.cs
+++ aspnet-core/src/pogoshift.EntityFrameworkCore/Migrations/20200421191519_Add-Data-To-Tenants.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pogoshift.EntityFrameworkCore;
 
 namespace pogoshift.Migrations
 {
     [DbContext(typeof(pogoshiftDbContext))]
-    partial class pogoshiftDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200421191519_Add-Data-To-Tenants")]
+    partial class AddDataToTenants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/pogoshift.EntityFrameworkCore/Migrations/20200421191519_Add-Data-To-Tenants.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pogoshift.Migrations
+{
+    public partial class AddDataToTenants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine1",
+                table: "AbpTenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine2",
+                table: "AbpTenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DailyAssociateMinimum",
+                table: "AbpTenants",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DailyManagerMinimum",
+                table: "AbpTenants",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AbpTenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "AbpTenants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StoreNumber",
+                table: "AbpTenants",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine1",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine2",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "DailyAssociateMinimum",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "DailyManagerMinimum",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "StoreNumber",
+                table: "AbpTenants");
+        }
+    }
+}

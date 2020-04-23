d3feadc74480fab5678040f42eb6e4494f213f3f
Joe Boris <josephdboris@gmail.com>
2020-04-23 13:53:09 -0400

    Added new User data to the RegisterViewModel

--- aspnet-core/src/pogoshift.Web.Mvc/Models/Account/RegisterViewModel.cs
+++ aspnet-core/src/pogoshift.Web.Mvc/Models/Account/RegisterViewModel.cs
@@ -1,14 +1,23 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Abp.Auditing;
 using Abp.Authorization.Users;
 using Abp.Extensions;
 using pogoshift.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace pogoshift.Web.Models.Account
 {
     public class RegisterViewModel : IValidatableObject
     {
+        [Required]
+        [StringLength(AbpUserBase.MaxUserNameLength)]
+        public string UserName { get; set; }
+
+        [StringLength(AbpUserBase.MaxPlainPasswordLength)]
+        [DisableAuditing]
+        public string Password { get; set; }
+
         [Required]
         [StringLength(AbpUserBase.MaxNameLength)]
         public string Name { get; set; }
@@ -17,17 +26,30 @@ namespace pogoshift.Web.Models.Account
         [StringLength(AbpUserBase.MaxSurnameLength)]
         public string Surname { get; set; }
 
-        [StringLength(AbpUserBase.MaxUserNameLength)]
-        public string UserName { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
 
         [Required]
         [EmailAddress]
         [StringLength(AbpUserBase.MaxEmailAddressLength)]
         public string EmailAddress { get; set; }
 
-        [StringLength(AbpUserBase.MaxPlainPasswordLength)]
-        [DisableAuditing]
-        public string Password { get; set; }
+        [Required]
+        [Phone]
+        [StringLength(AbpUserBase.MaxPhoneNumberLength)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string AddressLine1 { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string AddressLine2 { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string PostalCode { get; set; }
 
         public bool IsExternalLogin { get; set; }
 
